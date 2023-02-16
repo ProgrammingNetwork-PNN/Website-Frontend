946df49cc96faafb56cf521cc888ade96c14ced6
현수 <znight1020@naver.com>
2023-02-16 20:54:25 +0900

    FEAT: 동아리부원 소개
    - ClubMember.js 추가하여 동아리 부원 소개했습니다.
    - defalut로 토글창을 열어 놓았고 학년별 소개로 진행할까 합니다.

--- client/pnn/src/components/view/LandingPage/GuestLandingPage.js
+++ client/pnn/src/components/view/LandingPage/GuestLandingPage.js
@@ -3,10 +3,9 @@ import "./Landing.css";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import {
-  RecordVoiceOverOutlined,
-  GroupsOutlined,
-  SchoolOutlined,
-  MoreHorizOutlined,
+    GroupsOutlined,
+    SchoolOutlined,
+    MoreHorizOutlined,
 } from "@mui/icons-material";
 //import RecordVoice, SpatialAudio
 import { CardActionArea } from "@mui/material";
@@ -14,72 +13,88 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { useNavigate } from "react-router-dom";
 import {
-  Animator,
-  ScrollContainer,
-  ScrollPage,
-  batch,
-  MoveOut,
-  Sticky,
-  Fade,
+    Animator,
+    ScrollContainer,
+    ScrollPage,
+    batch,
+    MoveOut,
+    Sticky,
+    Fade,
 } from "react-scroll-motion";
 
+import ClubMember from "./ClubMember";
+
 const text =
-  "Programming을 즐기는 네트워크라는 뜻을 가진 \n인제대학교 컴퓨터공학부 학술 동아리입니다.";
+    "Programming을 즐기는 네트워크라는 뜻을 가진 \n인제대학교 컴퓨터공학부 학술 동아리입니다.";
 
 function Intro() {
-  return (
-    <div className="bg">
-      <video className="video" autoPlay loop muted>
-        <source src={process.env.PUBLIC_URL + "/images/file.mp4"} />
-      </video>
-      <div
-        className="text"
-        style={{ color: "#222", fontSize: "3rem", textAlign: "left" }}
-      >
-        P&N<p className="subTitle">{text}</p>
-      </div>
-      <div className="text1">둘러보기 ▼</div>
-    </div>
-  );
+    return (
+        <div className="bg">
+            <video className="video" autoPlay loop muted>
+                <source src={process.env.PUBLIC_URL + "/images/file.mp4"} />
+            </video>
+            <div
+                className="text"
+                style={{ color: "#222", fontSize: "3rem", textAlign: "left" }}
+            >
+                P&N<p className="subTitle">{text}</p>
+            </div>
+            <div className="text1">둘러보기 ▼</div>
+        </div>
+    );
 }
 
 function GuestLandingPage() {
-  const [img] = useState(["세미나.png", "coding.jpg", "onlineLecture.jpg"]);
-  const [cardTitle] = useState(["세미나", "팀프로젝트", "기타(동아리 활동)"]);
-  const [card] = useState(["semina", "team", "etc"]);
-  return (
-    <div>
-      <Intro></Intro>
-      <ScrollContainer>
-        {/*snap="mandatory"*/}
-        <></>
-        <ScrollPage>
-          <Animator animation={batch(Fade(), Sticky(), MoveOut(0, 20))}>
-            <p className="activity">Activity</p>
+    const [img] = useState(["세미나.png", "coding.jpg", "onlineLecture.jpg"]);
+    const [cardTitle] = useState(["세미나", "팀프로젝트", "기타(동아리 활동)"]);
+    const [card] = useState(["semina", "team", "etc"]);
+    return (
+        <div>
+            <Intro></Intro>
+            <ScrollContainer>
+                {/*snap="mandatory"*/}
+                <></>
+                <ScrollPage>
+                    <Animator
+                        animation={batch(Fade(), Sticky(), MoveOut(0, 20))}
+                    >
+                        <p className="activity">Activity</p>
 
-            <div className="card">
-              <Box sx={{ flexGrow: 3 }}>
-                <Grid container spacing={4}>
-                  {card.map((a, i) => {
-                    return (
-                      <Grid item lg={4} md={4} xs={4} key={i}>
-                        <GuestCard cardTitle={cardTitle} img={img} i={i} />
-                      </Grid>
-                    );
-                  })}
-                </Grid>
-              </Box>
-            </div>
-          </Animator>
-        </ScrollPage>
-        <ScrollPage>
-          <div style={{ lineHeight: "100vh" }}>
-            끝, 부원 설명하면 좋을듯? && Footer 넣을까 말까 고민중...
-          </div>
-        </ScrollPage>
-      </ScrollContainer>
-    </div>
-  );
+                        <div className="card">
+                            <Box sx={{ flexGrow: 3 }}>
+                                <Grid container spacing={4}>
+                                    {card.map((a, i) => {
+                                        return (
+                                            <Grid
+                                                item
+                                                lg={4}
+                                                md={4}
+                                                xs={4}
+                                                key={i}
+                                            >
+                                                <GuestCard
+                                                    cardTitle={cardTitle}
+                                                    img={img}
+                                                    i={i}
+                                                />
+                                            </Grid>
+                                        );
+                                    })}
+                                </Grid>
+                            </Box>
+                        </div>
+                    </Animator>
+                </ScrollPage>
+                <ScrollPage>
+                    <ClubMember
+                        title="Club Member"
+                        content="This is a content"
+                    />
+                    <div style={{ lineHeight: "100vh" }}></div>
+                </ScrollPage>
+            </ScrollContainer>
+        </div>
+    );
 }
 
 export default GuestLandingPage;
