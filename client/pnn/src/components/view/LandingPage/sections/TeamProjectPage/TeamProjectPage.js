fb439ae023d92a8c360ad57bdca771ada649b31d
Earthwarm_man <woals8115@naver.com>
2023-02-21 22:02:11 +0900

    파일 합치기

--- client/pnn/src/components/view/LandingPage/sections/TeamProjectPage/TeamProjectPage.js
+++ client/pnn/src/components/view/LandingPage/sections/TeamProjectPage/TeamProjectPage.js
@@ -1,7 +1,34 @@
-import React from "react";
 
+import React, { useState } from "react";
+import AcademicProject from "./Tpp/AcademicProject";
+import WebProject from "./Tpp/WebProject";
+
+import { ScrollContainer } from "react-scroll-motion";
+import { useNavigate } from "react-router-dom";
+import "./Tpp/Tpp.css";
 function TeamProjectPage() {
-  return <div>TeamProjectPage</div>;
+  let navigate = useNavigate();
+  let [num, setNum] = useState(0);
+  return (
+    <>
+    <button  onClick={()=>{setNum(0)}} class="titleBt">학술제</button>   
+    <button onClick={()=>{setNum(1)}} class="titleBtWeb">동아리 웹</button>
+    <hr></hr>
+    <TabPage tab={num}></TabPage>
+    
+    </>
+  );
+}
+function TabPage(props) {
+  return (
+    <div>
+      {props.tab == 0 ? (
+        <AcademicProject></AcademicProject>
+      ) : (
+        <WebProject></WebProject>
+      )}
+    </div>
+  );
 }
 
 export default TeamProjectPage;
