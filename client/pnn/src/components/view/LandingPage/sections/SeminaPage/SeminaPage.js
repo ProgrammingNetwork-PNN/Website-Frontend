61cb57e33d12053b7e8ddcd8f913e191e55af80d
현수 <znight1020@naver.com>
2023-02-27 03:02:26 +0900

    fix: package.json

--- client/pnn/src/components/view/LandingPage/sections/SeminaPage/SeminaPage.js
+++ client/pnn/src/components/view/LandingPage/sections/SeminaPage/SeminaPage.js
@@ -11,62 +11,80 @@ function Semina() {
     "DB 세미나를 통하여 데이터베이스의\n설계와 기초원리를 배울 수 있습니다.",
   ]);
   return (
-    <div className="is-preload" ref={scrollRef} style={{ overflow: "auto"}}>
-      <div id="page-wrapper" style={{paddingTop: '3vmax', paddingBottom: '0.5vmax'}}>
-        <div id="wrapper" style={{height: "60%"}}>
-          <section className="panel banner right">
-            <div className="content color0 span-3-75" style={{border: '0'}}>
-              <h1 className="major">
-                P&N <br />
-                Seminar
-              </h1>
-            </div>
-          </section>
+      <div className="is-preload" style={{ overflow: "auto" }}>
+          <div
+              id="page-wrapper"
+              style={{
+                  paddingTop: "3vmax",
+                  paddingBottom: "0.5vmax",
+                  margin: "auto",
+                  alignItems: "center",
+                  alignContent: "center",
+              }}
+          >
+              <div id="wrapper" style={{ height: "60%" }}>
+                  <section className="panel banner right">
+                      <div
+                          className="content color0 span-3-75"
+                          style={{ border: "0", width: "100%" }}
+                      >
+                          <h1 className="major">
+                              P&N <br />
+                              Seminar
+                          </h1>
+                      </div>
+                  </section>
 
-          {/*****************************1******************************/}
-          <section className="panel spotlight medium left">
-            <div className="content span-7" style={{border: '0'}}>
-              <h2 className="major">C/C++ Seminar</h2>
-              <img
-                width="469px"
-                height="350px"
-                src={`${process.env.PUBLIC_URL}/images/seminar1.png`}
-              ></img>
-            </div>
-            <div className="image filtered tinted"></div>
-            <p className="myPFont">{messages[0]}</p>
-          </section>
-          {/*****************************2******************************/}
-          <section className="panel spotlight medium left">
-            <div className="content span-7" style={{border: '0'}}>
-              <h2 className="major">JAVA Seminar</h2>
-              <img
-                width="469px"
-                height="350px"
-                src={`${process.env.PUBLIC_URL}/images/seminar2.png`}
-              ></img>
-            </div>
-            <div className="image filtered tinted" data-position="top "></div>
-            <p className="myPFont">{messages[1]}</p>
-          </section>
-          {/*****************************3******************************/}
-          <section className="panel spotlight medium left">
-            <div className="content span-6" style={{border: '0'}}>
-              <h2 className="major">DB Seminar</h2>
-              <img
-                width="469px"
-                height="350px"
-                src={`${process.env.PUBLIC_URL}/images/seminar3.png`}
-              ></img>
-            </div>
-            <div className="image filtered tinted" data-position="top "></div>
-            <p className="myPFont">{messages[2]}</p>
-          </section>
+                  {/*****************************1******************************/}
+                  <section className="panel spotlight medium left">
+                      <div className="content span-7" style={{ border: "0" }}>
+                          <h2 className="major">C/C++ Seminar</h2>
+                          <img
+                              width="469px"
+                              height="350px"
+                              src={`${process.env.PUBLIC_URL}/images/seminar1.png`}
+                          ></img>
+                      </div>
+                      <div className="image filtered tinted"></div>
+                      <p className="myPFont">{messages[0]}</p>
+                  </section>
+                  {/*****************************2******************************/}
+                  <section className="panel spotlight medium left">
+                      <div className="content span-7" style={{ border: "0" }}>
+                          <h2 className="major">JAVA Seminar</h2>
+                          <img
+                              width="469px"
+                              height="350px"
+                              src={`${process.env.PUBLIC_URL}/images/seminar2.png`}
+                          ></img>
+                      </div>
+                      <div
+                          className="image filtered tinted"
+                          data-position="top "
+                      ></div>
+                      <p className="myPFont">{messages[1]}</p>
+                  </section>
+                  {/*****************************3******************************/}
+                  <section className="panel spotlight medium left">
+                      <div className="content span-6" style={{ border: "0" }}>
+                          <h2 className="major">DB Seminar</h2>
+                          <img
+                              width="469px"
+                              height="350px"
+                              src={`${process.env.PUBLIC_URL}/images/seminar3.png`}
+                          ></img>
+                      </div>
+                      <div
+                          className="image filtered tinted"
+                          data-position="top "
+                      ></div>
+                      <p className="myPFont">{messages[2]}</p>
+                  </section>
 
-          <section style={{ paddingLeft: "20px" }}></section>
-        </div>
+                  <section style={{ paddingLeft: "20px" }}></section>
+              </div>
+          </div>
       </div>
-    </div>
   );
 }
 
